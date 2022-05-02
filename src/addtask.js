1d4c3e8adfe74a4abfb53e45bf36ba73b5a32fbc
Ogaga <iyaraogaga69@gmail.com>
2022-05-02 15:11:21 +0100

    Add all project files

---
+++ src/addtask.js
@@ -0,0 +1,23 @@
+import { setStorage } from './localstorage.js';
+
+export default (arr) => {
+  const addTask = document.getElementById('input-task').value;
+  let taskIndex = 0;
+
+  arr.forEach((element) => {
+    taskIndex = element.index;
+  });
+  if (addTask.length === 0) {
+    alert('it cannot be empty');
+    return arr;
+  }
+  const newTask = {
+    description: addTask,
+    completed: false,
+    index: taskIndex + 1,
+  };
+  document.getElementById('input-task').value = '';
+  arr.push(newTask);
+  setStorage(arr);
+  return arr;
+};
